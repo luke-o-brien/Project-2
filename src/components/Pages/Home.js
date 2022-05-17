6aad1dc59b606165eb599d25e70e38a3cafcad01
Luke O'Brien <lukeobrien02@gmail.com>
2022-05-17 21:36:11 +0100

    Working fish of the day display on homescreen

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -7,6 +7,7 @@ function Home () {
     <>
       <h2> About</h2>
       <p>This website allows you to see how climate change and overfishing has affected the fish population of the world</p>
+      <p>Click on the links in the navbar above to take a look</p>
       <Dailyfish />
       <Footer />
     </>
--- src/components/UI/Dailyfish.js
+++ src/components/UI/Dailyfish.js
@@ -6,10 +6,11 @@ function Dailyfish() {
   // const [fishdex, setFishdex] = useState(0)
   // let [currentDate, setCurrentDate] = useState("")
   // const [currentFish, setCurrentFish] = useState("")
-  const [fishDisplay, setFishDisplay] = useState([])
+  
   let fishdex = 0
   let currentDate = "00/00/0000"
   let currentFish = ""
+  const [displayFish, setDisplayFish] = useState(undefined);
 
   useEffect(() => {
     
@@ -20,72 +21,46 @@ function Dailyfish() {
         localStorage.setItem("date", currentDate);
         localStorage.setItem("current fish", currentFish)
         getRandom()
-        console.log(currentFish)
       } else if (localStorage.getItem("date") === currentDate) {
+        const getFish = localStorage.getItem("current fish")
+        const storageFish = JSON.parse(getFish)
+        setDisplayFish(storageFish)
         console.log("fish of the day is correct")
-        const displayFish = localStorage.getItem("current fish")
-        console.log(JSON.parse(displayFish))
+        console.log(storageFish)
+        console.dir(storageFish)
       } else {
         getRandom()
       }
     } 
   
     async function getRandom () {
-      let fishdex = Math.floor(Math.random() * 115);
+      const fishdex = Math.floor(Math.random() * 115);
       const res = await fetch("https://cryptic-everglades-76066.herokuapp.com/https://www.fishwatch.gov/api/species")
       const data = await res.json()
       console.log(data)
       const dailyfish = data[fishdex]
       console.log(dailyfish)
+      setDisplayFish(dailyfish)
       console.log(dailyfish["Species Name"])
       localStorage.setItem("current fish", JSON.stringify(dailyfish));
+      
     }
-    
-    
-    
-    // function getDate() {
-    //   const today = new Date().toLocaleDateString();
-    //   setCurrentDate(today)
-    //   console.log(currentDate)
-    //   if (localStorage.getItem("date") === null || localStorage.getItem("current fish" === null)) {
-    //     console.log("no date in local storage")
-    //     localStorage.setItem("date", currentDate);
-    //     localStorage.setItem("current fish", currentFish)
-    //   } else {
-    //     console.log("date in local storage")
-    //     const displayFish = localStorage.getItem("current fish")
-    //     console.log(JSON.parse(displayFish))
-    //     getRandom()
-    //   }
-    // }
-
-    // async function getRandom () {
-    //   const fishnumber = Math.floor(Math.random() * 115);
-    //   const fishindex = fishnumber
-    //   setFishdex(fishindex)
-    //   console.log(fishdex)
-    //   const res = await fetch("https://cryptic-everglades-76066.herokuapp.com/https://www.fishwatch.gov/api/species")
-    //   const data = await res.json()
-    //   console.log(data)
-    //   const dailyfish = data[fishdex]
-    //   console.log(dailyfish)
-    //   localStorage.setItem("current fish", JSON.stringify(dailyfish));
-    // }
     getDate()
   }, [])
 
-  return (
+  return displayFish ?
     <>
       <h2>Fish of The Day</h2>
-      <h3></h3>
-      <img></img>
+      <h3>{displayFish["Species Name"]}</h3>
+      <img src={displayFish["Species Illustration Photo"].src}/>    
+      <p>{displayFish.Population}</p> 
       <ul>
         <li>
           Key facts
         </li>
       </ul>
-    </>
-  )
+    </> : <p>Waiting for information</p>
+
 }
 
 export default Dailyfish
